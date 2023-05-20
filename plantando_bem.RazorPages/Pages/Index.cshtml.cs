8caf9890302c8ab31a1a9dbc7c0e5beeb39cff52
dbonfleur <danibonfleur@hotmail.com>
2023-05-20 17:07:11 -0300

    initial commit
    Realizado a construção base do projeto aonde estará o escopo para o desenvolvimento, criado Razor Pages, e a API responsável pela persistência de dados.

---
+++ plantando_bem.RazorPages/Pages/Index.cshtml.cs
@@ -0,0 +1,17 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace plantando_bem.RazorPages.Pages
+{
+    public class Index : PageModel
+    {
+
+        public Index()
+        {
+            
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
